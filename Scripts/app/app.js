7319060c586e9231d1699c8962542f198289abb5
Martinez Avendano <c.martinez.avendano@accenture.com>
2017-01-20 14:13:26 -0600

    Adding scripts

---
+++ Scripts/app/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+
+        $routeProvider.when('/simplePaging', {
+            templateUrl: '/Scripts/app/simple/simple.html',
+            controller: 'simpleCtrl',
+            caseInsensitiveMatch: true
+        });
+       
+    }]);
+
+    app.run([function () {
+    }]);
+})();
